88d8576bd8d43b91aba27b4ad3d94cedaf4f8b85
monsieurleberre <monsieur.leberre@gmail.com>
2018-02-28 15:04:01 +0000

    task 522 and task 519 refactor the SidechainInfo/SidechainInfoRequest and NetworkInfo/NetworkInfoRequest classes before adding a few new properties on them

---
+++ src/NBitcoin/Sidechains/NetworkInfoRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace NBitcoin
+{
+    public class NetworkInfoRequest
+    {
+        public uint Time { get; set; }
+
+        public uint Nonce { get; set; }
+
+        public int Port { get; set; }
+
+        public int RpcPort { get; set; }
+
+        public int AddressPrefix { get; set; }
+
+        public NetworkInfoRequest(uint time, uint nonce, int port, int rpcPort, int addressPrefix)
+        {
+            this.Time = time;
+            this.Nonce = nonce;
+            this.Port = port;
+            this.RpcPort = rpcPort;
+            this.AddressPrefix = addressPrefix;
+        }
+
+        public NetworkInfoRequest()
+        {
+
+        }
+    }
+}
---
+++ src/NBitcoin/Sidechains/SidechainInfoRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace NBitcoin
+{
+    public class SidechainInfoRequest : SidechainInfo<NetworkInfoRequest>
+    {
+        public SidechainInfoRequest(string name, NetworkInfoRequest mainNet, NetworkInfoRequest testNet, NetworkInfoRequest regTest)
+            : base(name, mainNet, testNet, regTest)
+        { }
+    }
+
+}
