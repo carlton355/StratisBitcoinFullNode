ec9f2c8d29c4e9a8d7f560ef5664025e6958e850
monsieurleberre <monsieur.leberre@gmail.com>
2018-03-01 10:37:53 +0000

    parameter rename

--- src/NBitcoin/Sidechains/SidechainInfoRequest.cs
+++ src/NBitcoin/Sidechains/SidechainInfoRequest.cs
@@ -2,9 +2,9 @@
 {
     public class SidechainInfoRequest : SidechainInfo<NetworkInfoRequest>
     {
-        public SidechainInfoRequest(string name, string coinName, int coinType, 
+        public SidechainInfoRequest(string chainName, string coinName, int coinType, 
             NetworkInfoRequest mainNet, NetworkInfoRequest testNet, NetworkInfoRequest regTest)
-            : base(name, coinName, coinType, mainNet, testNet, regTest)
+            : base(chainName, coinName, coinType, mainNet, testNet, regTest)
         { }
     }
 
